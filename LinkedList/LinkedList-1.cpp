ed8168039ff1495b1e8a87dea7c2059e9a95d90e
Govind Pathak <govindpathak@Govinds-MacBook-Pro.local>
2026-03-21 12:35:05 +0530

    Printing a linked list

---
+++ LinkedList/LinkedList-1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = nullptr;
+    }
+};
+
+int main(){
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    // Print the linked list
+    Node *curr = head;
+    while(curr != nullptr){
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+
+    return 0;
+}
